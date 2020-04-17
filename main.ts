4242dc48c9724b93549b0dce61ea9af1a3eb4184
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-04-17 12:52:49 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,25 @@
-input.buttonD12.onEvent(ButtonEvent.Click, function () {
+input.buttonD4.onEvent(ButtonEvent.Click, function () {
     clik = !(clik)
     if (clik) {
-        pixel.setColor(0x007fff)
+    	
     } else {
-        pixel.setColor(0xff0000)
+    	
     }
 })
 let clik = false
 clik = false
 forever(function () {
     if (clik) {
+        pixel.setColor(0x007fff)
         pins.A1.digitalWrite(true)
-        pins.D13.digitalWrite(true)
         mouse.setButton(MouseButton.Left, true)
         pause(60)
         mouse.setButton(MouseButton.Left, false)
         pins.A1.digitalWrite(false)
-        pins.D13.digitalWrite(false)
+        pixel.setColor(0x000000)
+    } else {
+        pixel.setColor(0xff0000)
+        pins.A1.digitalWrite(true)
     }
     pause(100)
 })
